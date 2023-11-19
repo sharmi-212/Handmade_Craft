75c040932845808c3e9e04d29d490940ded65382
sharmi-212 <118599027+sharmi-212@users.noreply.github.com>
2023-11-19 18:36:25 +0530

    Update LoginPage.jsx

--- src/components/Footer/LoginPage.jsx
+++ src/components/Footer/LoginPage.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './LoginPage.css';
-// import loginImage from './login.jpg'; // Import your login image
 
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   
   const navigate = useNavigate();
-  const handleLogin = async (e) => {
+  
+  const handleLogin = (e) => {
     e.preventDefault();
 
     if (!username || !password) {
@@ -18,22 +18,15 @@ function LoginPage() {
     }
 
     try {
-      const response = await fetch('/coachlog', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
+      // Check if the password is "123"
+      if (password === '123') {
+        // Simulate a successful login
+        localStorage.setItem('token', 'fakeToken'); // Set a fake token for demonstration purposes
 
-      if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem('token', data.token);
-   
-        alert('Login Successfull');
+        alert('Login Successful');
         navigate("/");
       } else {
-        alert('Invalid username or password'); 
+        alert('Invalid password'); 
       }
     } catch (error) {
       console.error('Error:', error);
@@ -44,47 +37,40 @@ function LoginPage() {
 
   return (
     <div className='log'>
-    <div className="login-container" >
-      <div className="login-content">
-        <div className="login-form">
-          <h2>Sign In</h2>
-          <p>“ we've got this!”</p>
-          <form onSubmit={handleLogin}>
-            <div className="form-group">
-              
-            <input
+      <div className="login-container" >
+        <div className="login-content">
+          <div className="login-form">
+            <h2>Sign In</h2>
+            <p>“ we've got this!”</p>
+            <form onSubmit={handleLogin}>
+              <div className="form-group">
+                <input
                   type="text"
                   id="input1"
                   name="input1"
-                
                   required
                   placeholder="Username"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                 />
-            </div>
-            <div className="form-group">
-            <input
+              </div>
+              <div className="form-group">
+                <input
                   type="password"
                   id="input2"
                   name="input2"
-               
                   required
                   placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
-            </div>
-            <button type="submit">Login</button><br/><br/>
-            Don't have a account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
-          </form>
+              </div>
+              <button type="submit">Login</button><br/><br/>
+              Don't have an account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
+            </form>
+          </div>
         </div>
-        {/* <div className="login-image">
-          <img src={loginImage} alt="Login" />
-         </div>
-         */}
       </div>
-    </div>
     </div>
   );
 }
